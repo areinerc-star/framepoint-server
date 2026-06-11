7db68cea384a4297f87e7cd1c3b347ec27b53d7d
framepointphtgrphy-maker <framepointphtgrphy@gmail.com>
2026-06-11 12:53:36 +0800

    Update servev9r.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(cors({
   origin: '*',
-  methods: ['GET', 'POST', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'x-admin-token'],
 }));
 app.options('*', cors());
